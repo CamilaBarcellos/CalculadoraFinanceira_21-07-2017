4bd1dc432260136d666685ca8092f963dbc7924a
camila <camilabarcellos.car@gmail.com>
2017-07-21 12:39:38 -0300

    Inclusão de projeto

---
+++ CalculadoraFinanceira_21-07-2017/CalcularInvestimentos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class CalcularInvestimentos
+    {
+        public double ValorAplicado { get; set; }
+        public double TaxaJuros { get; set; }
+        public double Rendimento { get; set; }
+        public double ValorTotalPoupanca { get; set; }
+        public double ValorTotalFixa { get; set; }
+        public int TempoInvestimento { get; set; }
+        public double RendimentoFixa { get; set; }
+        public double TaxaJurosFixa { get; set; }
+        public double ImpostoDeRenda { get; set; }
+        public double ValorIr { get; set; }
+        public double ValorRendimentoFixa { get; set; }
+
+
+
+        public void CalcularRendimentoPoupanca()
+        {
+            ValorTotalPoupanca = ValorAplicado;
+
+            for (var i = 1; i <= TempoInvestimento; i++)
+            {
+                Rendimento = ValorTotalPoupanca * (TaxaJuros / 100);
+                ValorTotalPoupanca += Rendimento;
+            }
+
+        }
+
+        public void CalcularRendimentoFixa()
+        {
+            ValorTotalFixa = ValorAplicado;
+
+            for (var i = 1; i <= TempoInvestimento; i++)
+            {
+                RendimentoFixa = ValorTotalFixa * (TaxaJurosFixa / 100);
+                ValorTotalFixa += RendimentoFixa;
+            }
+            ValorRendimentoFixa = ValorTotalFixa - ValorAplicado;
+        }
+
+        public void CalcularImpostoDeRenda()
+        {
+            if (TempoInvestimento <= 12)
+            {
+                ImpostoDeRenda = ValorRendimentoFixa * 0.25;
+            }
+
+            else if (TempoInvestimento <= 24)
+            {
+                ImpostoDeRenda = ValorRendimentoFixa * 0.15;
+            }
+
+            else if (TempoInvestimento <= 36)
+            {
+                ImpostoDeRenda = ValorRendimentoFixa * 0.05;
+            }
+
+            else
+            {
+                ImpostoDeRenda = ValorRendimentoFixa * 0.01;
+            }
+        }
+
+        public void CalcularValorTotalFixaComIr()
+        {
+            ValorIr = ValorAplicado + (ValorRendimentoFixa - ImpostoDeRenda);
+        }
+
+    }
+}
--- CalculadoraFinanceira_21-07-2017/Program.cs
+++ CalculadoraFinanceira_21-07-2017/Program.cs
@@ -4,12 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CalculadoraFinanceira_21_07_2017
+namespace Teste
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CalcularInvestimentos calcular = new CalcularInvestimentos();
+
+            Console.WriteLine("Valor a ser aplicado:\n");
+            calcular.ValorAplicado = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nPercentual do rendimento mensal na POUPANÇA:\n");
+            calcular.TaxaJuros = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nPercentual do rendimento mensal FIXO:");
+            calcular.TaxaJurosFixa = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nTempo (em meses) da aplicacao:\n");
+            calcular.TempoInvestimento = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("\n------------------------------------------------------------------------\n");
+
+
+            calcular.CalcularRendimentoPoupanca();
+            calcular.CalcularRendimentoFixa();
+            calcular.CalcularImpostoDeRenda();
+            calcular.CalcularValorTotalFixaComIr();
+
+
+
+            Console.WriteLine("\nO valor investido na Poupança no final da aplicação será: R${0 :0.##}.\nNa modalidade de Renda Fixa será: R${1 :0.##}", calcular.ValorTotalPoupanca, calcular.ValorIr);
+
+            if (calcular.ValorTotalPoupanca > calcular.ValorIr)
+            {
+                Console.WriteLine("\nÉ mais indicado fazer o investimento na Poupança, pois o rendimento será maior.");
+            }
+            else if (calcular.ValorTotalPoupanca < calcular.ValorIr)
+            {
+                Console.WriteLine("\nÉ mais indicado fazer o investimento na Renda Fixa, pois o rendimento será maior.");
+            }
+            else
+            {
+                Console.WriteLine("\nO valor do rendimento será o mesmo para as duas modalidades de aplicação.");
+            }
+
+
+            Console.ReadLine();
+
         }
     }
 }
